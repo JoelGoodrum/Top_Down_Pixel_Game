448363efd4b70d3f2cfe8e9bfb55874191373f61
Joel Goodrum <joelgoodrum@hotmail.com>
2026-03-03 15:03:30 -0500

    run format

--- phaser-game/src/entities/PlayerState.ts
+++ phaser-game/src/entities/PlayerState.ts
@@ -1,16 +1,16 @@
 export class PlayerState {
-	private money: number
-	constructor(initialCash = 200) {
-		this.money = initialCash
-		this.items = ['banana', 'key']
-	}
-	getMoney() {
-		return this.money
-	}
-	addItem(item: string) {
-		this.items.push(item)
-	}
-	getItems(){
-		return [this.items.join(", ")]
-	}
+  private money: number
+  constructor(initialCash = 200) {
+    this.money = initialCash
+    this.items = ['banana', 'key']
+  }
+  getMoney() {
+    return this.money
+  }
+  addItem(item: string) {
+    this.items.push(item)
+  }
+  getItems() {
+    return [this.items.join(', ')]
+  }
 }
--- phaser-game/src/ui/hud.ts
+++ phaser-game/src/ui/hud.ts
@@ -3,20 +3,25 @@ import type { PlayerState } from '../entities/PlayerState'
 import { DEPTH } from '../config/constants'
 
 export type Hud = {
-	sync: () => void
+  sync: () => void
 }
 export function createHud(scene: Phaser.Scene, playerState: PlayerState): Hud {
-	const cashText = scene.add.text(12,12, '', {fontSize: '32px', fill: '#000'}).setScrollFactor(0).setDepth(DEPTH.UI)
-	const itemsText = scene.add.text(12,44, '', {fontSize: '32px', fill: '#000'}).setScrollFactor(0).setDepth(DEPTH.UI)
-	const sync = () => {
-		cashText.setText(`Cash: $${playerState.getMoney()}`)
-		itemsText.setText(`Items: ${playerState.getItems()}`)
-	}
+  const cashText = scene.add
+    .text(12, 12, '', { fontSize: '32px', fill: '#000' })
+    .setScrollFactor(0)
+    .setDepth(DEPTH.UI)
+  const itemsText = scene.add
+    .text(12, 44, '', { fontSize: '32px', fill: '#000' })
+    .setScrollFactor(0)
+    .setDepth(DEPTH.UI)
+  const sync = () => {
+    cashText.setText(`Cash: $${playerState.getMoney()}`)
+    itemsText.setText(`Items: ${playerState.getItems()}`)
+  }
 
-	sync()
+  sync()
 
-	return {
-		sync
-	}
-
+  return {
+    sync,
+  }
 }
